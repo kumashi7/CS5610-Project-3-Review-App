b0f823d357a5f2b08e3b4efd72eac8cc1ee61d5d
Mingming Zheng <mingmingzheng@mingmingdembp.lan>
2022-04-27 17:57:18 -0700

    init branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 
-import './App.css'
+import './App.css';
+
+import Entry from './components/entry/Entry.jsx';
 
 function App() {
     return (
-        <div>
-            <h1>This is a test</h1>
-        </div>
+        <main>
+            <Router>
+                <nav>
+                    <Link to='/entry'> Create Entry</Link>
+                </nav>
+            <Routes>
+                <Route path='/entry' element={<Entry/>} />
+            </Routes>
+            <div>
+                <h1>Home Page</h1>
+            </div>
+            </Router>
+        </main>
     );
 }
 
---
+++ src/components/entry/Entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Entry() {
+  return (
+    <div>Entry hahahah</div>
+  )
+}
+
+export default Entry
