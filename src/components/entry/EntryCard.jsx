9e1273ef61a033daa2d92ae8656bd957d0bf0025
Jianan Wang <jianan.wang129@gamil.com>
2022-05-05 18:17:59 -0700

    add styles

--- src/components/entry/EntryCard.jsx
+++ src/components/entry/EntryCard.jsx
@@ -8,7 +8,7 @@ function EntryCard({list}) {
 
   return list.map(({title, genre, release, content, _id}) => 
   <Link to="/entryDetails" state ={{id: _id}}>
-    <div key={_id} className='container flex'>
+    <div key={_id} className="entry">
       <h2>{title}</h2>
       <hr></hr>
       <h3>{genre}</h3><h3>{release}</h3>
